d59b9f96ca1996d8b685d75056715e69af0e1c48
u-gohil <756D6D61@gmail.com>
2020-12-16 22:25:45 +0000

    Basic Q+A functionality

--- src/pages/mid.js
+++ src/pages/mid.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../template/layout';
 import QandA from '../components/qanda';
+import Data from '../data/data.json';
 
 const Mid = () => {
+  const [question, setQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+
+  const handleClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = question + 1;
+    nextQuestion < Data.easy.length
+      ? setQuestion(nextQuestion)
+      : setShowScore(true);
+  };
   return (
     <Layout>
       <QandA />
+      {showScore ? (
+        <h1>
+          You scored {score} out of {Data.mid.length} 🎉
+        </h1>
+      ) : (
+        <>
+          <h3>
+            Question {question + 1}/{Data.easy.length}
+          </h3>
+          <h3>{Data.mid[question].question}</h3>
+          <div className='container'>
+            {Data.mid[question].answers.map((answers) => (
+              <button
+                className='answer-button'
+                onClick={() => handleClick(answers.correct)}
+              >
+                {answers.ans}
+              </button>
+            ))}
+          </div>
+        </>
+      )}
     </Layout>
   );
 };
