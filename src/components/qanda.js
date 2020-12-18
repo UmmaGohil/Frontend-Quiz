34d53d6ed84c9da357db05dd9a40facffcbeb4b3
u-gohil <756D6D61@gmail.com>
2020-12-18 02:32:09 +0000

    Fixed

--- src/components/qanda.js
+++ src/components/qanda.js
@@ -1,15 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Data from '../data/data.json';
 import './qanda.css';
+
 const QandA = (props) => {
   const [question, setQuestion] = useState(0);
   const [score, setScore] = useState(0);
   const [showScore, setShowScore] = useState(false);
-  const [dataJson, setDataJson] = useState(Data);
-
-  useEffect(() => {
-    setDataJson(Data);
-  }, []);
 
   const handleClick = (isCorrect) => {
     if (isCorrect) {
@@ -25,17 +21,16 @@ const QandA = (props) => {
     <>
       {showScore ? (
         <h1>
-          You scored {score} out of {dataJson[props.level].length} 🎉
+          You scored {score} out of {Data[props.level].length} 🎉
         </h1>
       ) : (
         <>
           <h3>
-            Question {question + 1}/{dataJson[props.level].length}
+            Question {question + 1}/{Data[props.level].length}
           </h3>
-          <h3>{dataJson[props.level][question].question}</h3>
-
+          <h3>{Data[props.level][question].question}</h3>
           <div className='container'>
-            {dataJson[props.level][question].answers.map((answers) => (
+            {Data[props.level][question].answers.map((answers) => (
               <button
                 className='answer-button'
                 onClick={() => handleClick(answers.correct)}
